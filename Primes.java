8f37a3c1124a1af66eefc0d530875d684d6a0307
liaromano <lia.romano@post.runi.ac.il>
2024-12-03 10:50:57 +0200

    hw4test

--- Primes.java
+++ Primes.java
@@ -2,19 +2,39 @@ public class Primes {
     public static void main(String[] args) {
         // Replace this statement with your code
         int n= Integer.parseInt(args[0]);
-        Boolean [] prime= new Boolean [n+1];
+        boolean [] prime= new boolean [n+1];
         for(int i=0;i<prime.length;i++)
         {
-            if(i<1)
-            {
-                prime[i]=false;
-            }
-            if(i>1)
-            {
                 prime[i]=true;
+        }
+        for(int i=0;i<2;i++)
+        {
+                prime[i]=false;;
+        }
+        for(int i=2;i<prime.length;i++)
+        {
+            for(int j=2;j<prime.length;j++)
+            {
+                if(j>i)
+                    break;
+                if((i%j)==0 && i!=j)
+                {
+                    prime[i]=false;
+                }
             }
         }
-        
+        int count=0;
+        System.out.println("Prime numbers up to "+n+":");
+        for(int z=0;z<prime.length;z++)
+        {
+            if(prime[z]==true)
+            {
+                count++;
+                System.out.println(z);
+            }
+        }
+        int percent=(100*count)/n;
+        System.out.println("There are "+count+" primes between 2 and "+n+" ("+percent+"% are primes)");
 
     }
 }
